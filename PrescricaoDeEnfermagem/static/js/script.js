fbccd36fc057b7673e9ac051b08bad604ac6c2a6
Laura <lauracristina185@hotmail.com>
2017-04-23 21:37:31 -0300

    tela

---
+++ PrescricaoDeEnfermagem/static/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    var max_fields      = 10; //maximum input boxes allowed
+    var wrapper         = $("#input_prescricao"); //Fields wrapper
+    var add_button      = $("#add_button"); //Add button ID
+
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<tr><td></td></tr>'); //add input box
+        }
+    });
+
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
+});
